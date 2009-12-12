b5bfcf671f2f7276deeb6e58139f30ed3b3f2532
j0hnds <dj0hnve@gmail.com>
2009-12-11 17:46:26 -0700

    Added GPL licensing.

--- src/main/java/com/hephaestus/cron/SingleValueCronValue.java
+++ src/main/java/com/hephaestus/cron/SingleValueCronValue.java
@@ -1,4 +1,22 @@
 package com.hephaestus.cron;
+/*
+ * Copyright (c) 2009 Dave Sieh
+ *
+ * This file is part of CronUtils.
+ *
+ * CronUtils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CronUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CronUtils.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * Provides the implementation of a single Cron Value. This type of Cron Value
